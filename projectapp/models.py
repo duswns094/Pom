e0f63a724d0da1a9e6e075b0119962da2017d437
duswns094 <duswns094@gmail.com>
2021-11-29 17:34:45 +0900

    django course 42 commit

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,9 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 class Project(models.Model):
-
     title = models.CharField(max_length=20, null=False)
     image = models.ImageField(upload_to='project/', null=True)
     content = models.TextField(null=True)
