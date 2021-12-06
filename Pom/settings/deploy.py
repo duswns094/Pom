47641e4cceb6d96c251071c156a408f110ffad94
duswns094 <duswns094@gmail.com>
2021-12-06 13:34:39 +0900

    django secret

--- Pom/settings/deploy.py
+++ Pom/settings/deploy.py
@@ -1,13 +1,14 @@
 from .base import *
 
-def read_secret(secret_name):
 
+def read_secret(secret_name):
     file = open('/run/secrets/' + secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
 
     return secret
+
 
 env = environ.Env(
     # set casting, default value
@@ -21,13 +22,13 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
