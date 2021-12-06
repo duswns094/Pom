c6a07b32f3ae354f1af5c4508eebebb208617e06
duswns094 <duswns094@gmail.com>
2021-12-06 13:36:59 +0900

    django secret

--- Pom/settings/deploy.py
+++ Pom/settings/deploy.py
@@ -22,8 +22,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
